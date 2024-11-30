9749ce6ef7eaa92e71f6ec33d235dc7846be7e5a
scottboms <scottboms@users.noreply.github.com>
2024-11-30 16:44:10 +0000

    Fix styling

--- index.php
+++ index.php
@@ -31,77 +31,77 @@ if (! function_exists('sitemap')) {
 }
 
 Kirby::plugin(
-  name: 'bnomei/feed',
-  info: [
-    'homepage' => 'https://github.com/scottboms/kirby3-feed'
-  ],
-  version: '5.0.2',
-  extends: [
-    'options' => [
-        'defaults' => function () {
-            $site = site();
+    name: 'bnomei/feed',
+    info: [
+        'homepage' => 'https://github.com/scottboms/kirby3-feed',
+    ],
+    version: '5.0.2',
+    extends: [
+        'options' => [
+            'defaults' => function () {
+                $site = site();
 
-            return [
-                // atom/json/rss
-                'url' => $site->url(),
-                'feedurl' => $site->url().'/feed/',
-                'title' => 'Feed',
-                'description' => '',
-                'link' => $site->url(),
-                'urlfield' => 'url',
-                'titlefield' => 'title',
-                'idfield' => 'id',
-                'datefield' => 'date',
-                'textfield' => 'text',
-                'modified' => time(),
-                'snippet' => 'feed/rss',
-                'mime' => null,
-                'sort' => true,
-                // sitemap
-                'dateformat' => 'r', // rss => r, sitemap => c
-                'xsl' => true,
-                'images' => false,
-                'imagesfield' => 'images',
-                'imagetitlefield' => 'title',
-                'imagecaptionfield' => 'caption',
-                'imagelicensefield' => 'license',
-                'videos' => false,
-                'videosfield' => 'videos',
-                'videotitlefield' => 'title',
-                'videothumbnailfield' => 'thumbnail',
-                'videodescriptionfield' => 'description',
-                'videourlfield' => 'url',
-            ];
-        },
-        'cache' => true,
-        'expires' => (60 * 24 * 7), // minutes
-    ],
-    'snippets' => [
-        'feed/atom' => __DIR__.'/snippets/feed/atom.php',
-        'feed/json' => __DIR__.'/snippets/feed/json.php',
-        'feed/rss' => __DIR__.'/snippets/feed/rss.php',
-        'feed/sitemap' => __DIR__.'/snippets/feed/sitemap.php',
-        'feed/sitemapxsl' => __DIR__.'/snippets/feed/sitemap.xsl.php',
-    ],
-    'pagesMethods' => [
-        'feed' => function (array $options = []): Response {
-            return feed(fn () => $this, $options);
-        },
-        'sitemap' => function (array $options = []): Response {
-            return sitemap(fn () => $this, $options);
-        },
-    ],
-    'hooks' => [
-        'page.*:after' => function ($event, $page) {
-            if ($event->action() !== 'render') {
-                \Bnomei\Feed::flush();
-            }
-        },
-        'file.*:after' => function ($event, $file) {
-            if ($event->action() !== 'render') {
-                \Bnomei\Feed::flush();
-            }
-        },
-    ],
-  ]
+                return [
+                    // atom/json/rss
+                    'url' => $site->url(),
+                    'feedurl' => $site->url().'/feed/',
+                    'title' => 'Feed',
+                    'description' => '',
+                    'link' => $site->url(),
+                    'urlfield' => 'url',
+                    'titlefield' => 'title',
+                    'idfield' => 'id',
+                    'datefield' => 'date',
+                    'textfield' => 'text',
+                    'modified' => time(),
+                    'snippet' => 'feed/rss',
+                    'mime' => null,
+                    'sort' => true,
+                    // sitemap
+                    'dateformat' => 'r', // rss => r, sitemap => c
+                    'xsl' => true,
+                    'images' => false,
+                    'imagesfield' => 'images',
+                    'imagetitlefield' => 'title',
+                    'imagecaptionfield' => 'caption',
+                    'imagelicensefield' => 'license',
+                    'videos' => false,
+                    'videosfield' => 'videos',
+                    'videotitlefield' => 'title',
+                    'videothumbnailfield' => 'thumbnail',
+                    'videodescriptionfield' => 'description',
+                    'videourlfield' => 'url',
+                ];
+            },
+            'cache' => true,
+            'expires' => (60 * 24 * 7), // minutes
+        ],
+        'snippets' => [
+            'feed/atom' => __DIR__.'/snippets/feed/atom.php',
+            'feed/json' => __DIR__.'/snippets/feed/json.php',
+            'feed/rss' => __DIR__.'/snippets/feed/rss.php',
+            'feed/sitemap' => __DIR__.'/snippets/feed/sitemap.php',
+            'feed/sitemapxsl' => __DIR__.'/snippets/feed/sitemap.xsl.php',
+      ],
+        'pagesMethods' => [
+            'feed' => function (array $options = []): Response {
+                return feed(fn () => $this, $options);
+            },
+            'sitemap' => function (array $options = []): Response {
+                return sitemap(fn () => $this, $options);
+            },
+      ],
+        'hooks' => [
+            'page.*:after' => function ($event, $page) {
+                if ($event->action() !== 'render') {
+                    \Bnomei\Feed::flush();
+                }
+            },
+            'file.*:after' => function ($event, $file) {
+                if ($event->action() !== 'render') {
+                    \Bnomei\Feed::flush();
+                }
+            },
+      ],
+    ]
 );
